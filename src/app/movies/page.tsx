98b3e4da5d75b674d804bf0c90f46f1625daf9e5
jakobpalmborg <jakob.palmborg@gmail.com>
2023-04-25 16:04:04 +0200

    movies page with mock data

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,3 +1,58 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const MOCK = [
+  {
+    id: 1,
+    name: 'Encanto',
+    img: '/images/encanto.jpg',
+  },
+  {
+    id: 2,
+    name: 'Isle of dogs',
+    img: '/images/encanto.jpg',
+  },
+  {
+    id: 3,
+    name: 'Min granne Totoro',
+    img: '/images/encanto.jpg',
+  },
+  {
+    id: 4,
+    name: 'The Shawshank Redemption',
+    img: '/images/encanto.jpg',
+  },
+  {
+    id: 5,
+    name: 'Forrest Gump',
+    img: '/images/encanto.jpg',
+  },
+];
+
 export default function Movies() {
-  return <div>Movie Page</div>;
+  return (
+    <>
+      <h2 className="movies-h2">Alla filmer</h2>
+      <div className="movies-header-divider"></div>
+      <section className="sec-cont">
+        <ul className="movies-list-item-container">
+          {MOCK.map((movie) => {
+            return (
+              <li key={movie.id} className="movies-list-item">
+                <Link href="/movies">
+                  <Image
+                    src={movie.img}
+                    alt="movie image"
+                    width={202.5}
+                    height={300}
+                  />
+                  <h4 className="movie-title-text">{movie.name}</h4>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </>
+  );
 }
