5a1cf2b724374a79185d3a6f4109ebcd82ddf3ca
Fabrithegreat <fabrixio.rodrigo@gmail.com>
2023-04-26 22:26:26 +0200

    added screening model with a reference to the movie model

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,14 +1,6 @@
 import { db } from '@/utils/db';
-import User from '@/models/user.model';
 db();
 
 export async function GET(request: Request) {
-  const user = new User({
-    name: "Fabricios",
-    password: "admin",
-    email: "jag@fabricioflores.se"
-  });
-
-  const res = await user.save();
-  return new Response(JSON.stringify(res))
+  return new Response("Hello from NextJS!")
 }
---
+++ src/models/screening.model.ts
@@ -0,0 +1,25 @@
+import { Schema, model, models, Model, Document } from 'mongoose';
+
+export interface IScreening extends Document {
+    salon: string,
+    time: Date | string,
+    movie: string
+}
+
+const screeningSchema = new Schema({
+    salon: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: Date,
+        required: true
+    },
+    movie: {
+        type: Schema.Types.ObjectId,
+        ref: "Movie",
+        required: true
+    }
+});
+
+export default models.Screening as Model<IScreening> || model<IScreening>("Screening", screeningSchema);
