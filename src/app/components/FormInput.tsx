846d737fba7c6aa3499b8bb40d528def6b93c68e
mkor92 <mkorhonen92@gmail.com>
2023-04-28 11:17:17 +0200

    in progress

--- src/app/components/FormInput.tsx
+++ src/app/components/FormInput.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function FormInput({ label }) {
+export default function FormInput({ label, text }) {
   return (
     <div className="formInput">
-      <label htmlFor="" className="text">
-        {label}
+      <label htmlFor={label} className="text">
+        {text}
       </label>
       <input type="text" id={label} className="form-input-field" />
     </div>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,14 +3,15 @@ import FormInput from "../components/FormInput";
 
 export default function signup() {
   return (
-    <div className="registration sec sec-cont">
-      <form className="sec-cont">
+    <div className="registration sec-cont">
+      <form className="">
         <h2>Registrera dig</h2>
-        <FormInput label="För- och efternamn" />
-        <FormInput label="Email" />
-        <FormInput label="Bekräfta Email" />
-        <FormInput label="Lösenord" />
-        <FormInput label="Bekräfta lösenord" />
+        <FormInput text="För- och efternamn" label="name" />
+        <FormInput text="Email" label="Email" />
+        <FormInput text="Bekräfta Email" label="cEmail" />
+        <FormInput text="Lösenord" label="password" />
+        <FormInput text="Bekräfta lösenord" label="cPassword" />
+        <button className="primary-btn">Registrera</button>
       </form>
     </div>
   );
