180930e3b4b040f68006efb9b97ae01dac79ad57
mkor92 <mkorhonen92@gmail.com>
2023-04-28 14:26:48 +0200

    in progress

--- src/app/movies/[movieId]/page.tsx
+++ src/app/movies/[movieId]/page.tsx
@@ -1,38 +1,39 @@
 import React from "react";
+import { notFound } from "next/navigation";
 
 async function getMovie(id: any) {
-  const res = await fetch(`https://plankton-app-xhkom.ondigitalocean.app/api/movies/${id}`);
+  const res = await fetch(`api/movies/${id}`);
   return res.json();
 }
 
-export default async function movieInfo({ params: { movieId } }: any) {
-  console.log(`hej ${movieId}`);
-  const movieData = getMovie(movieId);
-  const [movie] = await Promise.all([movieData]);
-  console.log(movie);
-  return (
-    <>
-      <main className="sec">
-        <article className="movie sec-cont">
-          <div>
-            <h1 className="title">{movie.data.attributes.title}</h1>
-          </div>
-          <div className="movie-text">
-            <img
-              src={movie.data.attributes.image.url}
-              alt="movie image"
-              className="movie-img"></img>
-            <p className="intro">{movie.data.attributes.intro}</p>
-          </div>
-          <h2 className="rating sec-cont">Betyg</h2>
-        </article>
+export default async function movieInfo({ params: { movieId } }) {
+  const movie = await getMovie(movieId);
+  if (!movie.data) return notFound();
+  else {
+    return (
+      <>
+        <div className="sec">
+          <article className="movie sec-cont">
+            <div>
+              <h1 className="title">{movie.data.attributes.title}</h1>
+            </div>
+            <div className="movie-text">
+              <img
+                src={movie.data.attributes.image.url}
+                alt="movie image"
+                className="movie-img"></img>
+              <p className="intro">{movie.data.attributes.intro}</p>
+            </div>
+            <h2 className="rating sec-cont">Betyg</h2>
+          </article>
 
-        <section className="screenings sec-cont">
-          <h2>Här kommer filvisningar</h2>
-        </section>
-        <section className="review-container sec-cont"></section>
-        <section className="review-box sec-cont"></section>
-      </main>
-    </>
-  );
+          <section className="screenings sec-cont">
+            <h2>Här kommer filmvisningar</h2>
+          </section>
+          <section className="review-container sec-cont"></section>
+          <section className="review-box sec-cont"></section>
+        </div>
+      </>
+    );
+  }
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,17 +1,67 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 import FormInput from "../components/FormInput";
+import Link from "next/link";
 
 export default function signup() {
+  const [regData, setRegData] = useState({
+    name: String,
+    email: String,
+    password: String,
+  });
+
+  function handleName(e: any) {
+    //console.log(e.target.id);
+    e.preventDefault();
+    setRegData({ ...regData, name: e.target.value });
+  }
+  function handleEmail(e: any) {
+    e.preventDefault();
+    setRegData({ ...regData, email: e.target.value });
+  }
+
+  function handlePass(e: any) {
+    e.preventDefault();
+    setRegData({ ...regData, password: e.target.value });
+  }
+
   return (
     <div className="registration sec-cont">
       <form className="">
         <h2>Registrera dig</h2>
-        <FormInput text="För- och efternamn" label="name" />
-        <FormInput text="Email" label="Email" />
-        <FormInput text="Bekräfta Email" label="cEmail" />
-        <FormInput text="Lösenord" label="password" />
-        <FormInput text="Bekräfta lösenord" label="cPassword" />
-        <button className="primary-btn">Registrera</button>
+        <FormInput
+          type="text"
+          id="name"
+          text="För- och efternamn"
+          label="name"
+          onChange={handleName}
+        />
+        <FormInput type="email" id={null} onChange={null} text="Email" label="Email" />
+        <FormInput
+          type="email"
+          id="email"
+          text="Bekräfta Email"
+          label="cEmail"
+          onChange={handleEmail}
+        />
+        <FormInput type="password" id={null} onChange={null} text="Lösenord" label="password" />
+        <FormInput
+          type="password"
+          id="password"
+          text="Bekräfta lösenord"
+          label="cPassword"
+          onChange={handlePass}
+        />
+        <button
+          className="primary-btn"
+          onClick={() => {
+            console.log(regData);
+          }}>
+          Registrera
+        </button>
+        <Link href="/" className="cancel-btn">
+          Avbryt
+        </Link>
       </form>
     </div>
   );
