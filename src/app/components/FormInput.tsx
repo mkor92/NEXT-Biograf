3f90266c829f1626aae16dd6f8de1abc9814eaa1
mkor92 <mkorhonen92@gmail.com>
2023-04-28 14:26:41 +0200

    in progress

--- src/app/components/FormInput.tsx
+++ src/app/components/FormInput.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-export default function FormInput({ label, text }) {
+export default function FormInput({ label, text, onChange, id, type, errorMsg }) {
   return (
     <div className="formInput">
       <label htmlFor={label} className="text">
         {text}
       </label>
-      <input type="text" id={label} className="form-input-field" />
+      <input type={type} id={id} className="form-input-field" onChange={onChange} />
+      <span>{errorMsg}</span>
     </div>
   );
 }
