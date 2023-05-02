c975758428e84a0b8d27068ea3417d2f3e536e14
mkor92 <mkorhonen92@gmail.com>
2023-05-02 10:05:09 +0200

    in progress

--- src/app/components/FormInput.tsx
+++ src/app/components/FormInput.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-export default function FormInput({ label, text, onChange, id, type, errorMsg }) {
+export default function FormInput(props) {
+  { label, errorMsg, onChange, id, ...inputProps } = props;
   return (
     <div className="formInput">
       <label htmlFor={label} className="text">
@@ -11,3 +12,31 @@ export default function FormInput({ label, text, onChange, id, type, errorMsg })
     </div>
   );
 }
+
+
+/*const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
+  return (
+    <div className="formInput">
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
+};
+
+export default FormInput;*/
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -24,34 +24,61 @@ export default function signup() {
     e.preventDefault();
     setRegData({ ...regData, password: e.target.value });
   }
+  const inputs = [
+    {
+      id: 1,
+      name: "name",
+      type: "text",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character!",
+      label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      errorMessage: "It should be a valid email address!",
+      label: "E-postadress",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "cEmail",
+      type: "email",
+      errorMessage: "It should be a valid email address!",
+      label: "Bekräfta e-post",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      label: "Lösenord",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+    },
+    {
+      id: 5,
+      name: "cPassword",
+      type: "password",
+      placeholder: "Bekräfta lösenord",
+      errorMessage: "Passwords don't match!",
+      label: "Confirm Password",
+      pattern: values.password,
+      required: true,
+    },
+  ];
 
   return (
     <div className="registration sec-cont">
       <form className="">
         <h2>Registrera dig</h2>
-        <FormInput
-          type="text"
-          id="name"
-          text="För- och efternamn"
-          label="name"
-          onChange={handleName}
-        />
-        <FormInput type="email" id={null} onChange={null} text="Email" label="Email" />
-        <FormInput
-          type="email"
-          id="email"
-          text="Bekräfta Email"
-          label="cEmail"
-          onChange={handleEmail}
-        />
-        <FormInput type="password" id={null} onChange={null} text="Lösenord" label="password" />
-        <FormInput
-          type="password"
-          id="password"
-          text="Bekräfta lösenord"
-          label="cPassword"
-          onChange={handlePass}
-        />
+        {inputs.map((input) => (
+          <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange} />
+        ))}
         <button
           className="primary-btn"
           onClick={() => {
