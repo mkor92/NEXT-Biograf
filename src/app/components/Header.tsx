c4bcbd955c2606543c38588385a8fe6f66dfd14b
Fabrithegreat <fabrixio.rodrigo@gmail.com>
2023-05-09 21:25:43 +0200

    fixed the nav and header and added new logo

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,47 +1,89 @@
-import Link from "next/link";
+'use client';
+
+import { faBars, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+import { useState, useEffect, useRef } from 'react';
 
 export default function Header() {
-  const menu = [
-    {
-      label: "Hem",
-      link: "/",
-    },
-    {
-      label: "Om oss",
-      link: "/about",
-    },
-    {
-      label: "Biljettinfo",
-      link: "/UC",
-    },
-    {
-      label: "Presentkort",
-      link: "/UC",
-    },
-    {
-      label: "Logga in",
-      link: "/login",
-    },
-  ];
-  return (
-    <header>
-      <Link href="/" className="logo">
-        <img src="logo.png" alt="logo" />
-      </Link>
-      <div className="sec">
-        <div className="header-content-wrap sec-cont">
-          <nav className="header-nav">
-            <button className="header-nav-toggle"></button>
-            <menu className="header-menu">
-              {menu.map((li, index) => (
-                <li className="header-menu-item" key={index}>
-                  <Link href={li.link}>{li.label}</Link>
-                </li>
-              ))}
-            </menu>
-          </nav>
-        </div>
-      </div>
-    </header>
-  );
+	const menu = [
+		{
+			label: 'Hem',
+			link: '/',
+		},
+		{
+			label: 'Om oss',
+			link: '/about',
+		},
+		{
+			label: 'Biljettinfo',
+			link: '/UC',
+		},
+		{
+			label: 'Presentkort',
+			link: '/UC',
+		},
+		{
+			label: 'Logga in',
+			link: '/login',
+		},
+	];
+	const [openMenu, setOpenMenu] = useState<boolean>(false);
+	const [height, setHeight] = useState(0);
+	const header = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		setHeight(header?.current?.clientHeight!);
+		console.log(height);
+	}, [openMenu]);
+
+	return (
+		<header className="sec" ref={header}>
+			<div className="sec-cont header-content">
+				<Link href="/" className="logo">
+					<img src="logo.svg" alt="logo" />
+					<p>Luleå Bio</p>
+				</Link>
+				<menu
+					className={openMenu ? 'header-menu header-menu-open' : 'header-menu'}
+					style={openMenu && height ? { top: `${height}px` } : {}}
+				>
+					{menu.map((li, index) => (
+						<li className="header-menu-item" key={index}>
+							<Link
+								href={li.link}
+								onClick={() => {
+									setOpenMenu(!openMenu);
+								}}
+							>
+								{li.label}
+							</Link>
+						</li>
+					))}
+				</menu>
+
+				{!openMenu && (
+					<FontAwesomeIcon
+						icon={faBars}
+						className="header-bars"
+						color="white"
+						onClick={() => {
+							setOpenMenu(!openMenu);
+						}}
+					/>
+				)}
+
+				{openMenu && (
+					<FontAwesomeIcon
+						icon={faCircleXmark}
+						className="header-bars"
+						color="white"
+						onClick={() => {
+							setOpenMenu(!openMenu);
+						}}
+					/>
+				)}
+			</div>
+		</header>
+	);
 }
