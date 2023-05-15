667f44483675689dcc87ced35cb2786e00eac8a9
jakobpalmborg <jakob.palmborg@gmail.com>
2023-05-15 15:11:40 +0200

    test for choose screening

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,8 @@
 describe('template spec', () => {
 	it('passes', () => {
 		cy.visit('/');
+		cy.get('nav li:first').click();
+		cy.get('ul li:first').click();
+		cy.get('[data-testid="book-screening-btn"]:first').click();
 	});
 });
