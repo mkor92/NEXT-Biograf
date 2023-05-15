919ef68b8c00f3200f4c643e9f720540e8e1d79a
jakobpalmborg <jakob.palmborg@gmail.com>
2023-05-15 17:05:03 +0200

    write test for payment and recieve messege of booking complete

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
 describe('template spec', () => {
-	it('passes', () => {
+	it('visit site and book a ticket as a guest', () => {
 		// visit the site and click on "filmer" (movies)
 		cy.visit('/');
 		cy.get('nav li:first').click();
@@ -19,5 +19,10 @@ describe('template spec', () => {
 		cy.get('input:first').type('John Doe');
 		cy.get('input:last').type('john@doe.com');
 		cy.get('button').click();
+
+		// choose payment method and click pay
+		cy.get('[data-testid="payment-method"]:first').click();
+		cy.get('button').click();
+		cy.contains('Bokning bekräftad!');
 	});
 });
