692ef3ee9c661846cb8527515abe81474697772a
jakobpalmborg <jakob.palmborg@gmail.com>
2023-05-15 15:36:58 +0200

    click number of tickets seat and guest-btn

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -4,5 +4,8 @@ describe('template spec', () => {
 		cy.get('nav li:first').click();
 		cy.get('ul li:first').click();
 		cy.get('[data-testid="book-screening-btn"]:first').click();
+		cy.get('[data-testid="ticket-count-plus"]').click();
+		cy.get('.seat-available:first').click();
+		cy.get('.guest-btn').click();
 	});
 });
--- src/app/components/TicketCount.tsx
+++ src/app/components/TicketCount.tsx
@@ -1,26 +1,30 @@
 import { FC } from 'react';
 
 type Props = {
-  ticketCount: number;
-  onClickMinus: () => void;
-  onClickPlus: () => void;
+	ticketCount: number;
+	onClickMinus: () => void;
+	onClickPlus: () => void;
 };
 
 const TicketCount: FC<Props> = ({ ticketCount, onClickMinus, onClickPlus }) => {
-  return (
-    <>
-      <h2 className="header-two">Välj antal biljetter</h2>
-      <div className="number-of-tickets-container">
-        <div onClick={onClickMinus} className="plus-minus">
-          -
-        </div>
-        <div className="ticket-count">{ticketCount}</div>
-        <div onClick={onClickPlus} className="plus-minus">
-          +
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<h2 className="header-two">Välj antal biljetter</h2>
+			<div className="number-of-tickets-container">
+				<div onClick={onClickMinus} className="plus-minus">
+					-
+				</div>
+				<div className="ticket-count">{ticketCount}</div>
+				<div
+					data-testid="ticket-count-plus"
+					onClick={onClickPlus}
+					className="plus-minus"
+				>
+					+
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default TicketCount;
