a383ffdd8ce6905e458aa9cfd3c34d8a49676aea
jakobpalmborg <jakob.palmborg@gmail.com>
2023-05-15 16:47:47 +0200

    fill form and click continue

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,11 +1,23 @@
 describe('template spec', () => {
 	it('passes', () => {
+		// visit the site and click on "filmer" (movies)
 		cy.visit('/');
 		cy.get('nav li:first').click();
+
+		// click the first movie
 		cy.get('ul li:first').click();
+
+		// click the first screening
 		cy.get('[data-testid="book-screening-btn"]:first').click();
+
+		// choose number of tickets, seat and click guest button
 		cy.get('[data-testid="ticket-count-plus"]').click();
 		cy.get('.seat-available:first').click();
-		cy.get('.guest-btn').click();
+		cy.get('[data-testid="guest-btn"]').click();
+
+		// fill form and click continue
+		cy.get('input:first').type('John Doe');
+		cy.get('input:last').type('john@doe.com');
+		cy.get('button').click();
 	});
 });
--- src/app/(site)/tickets/page.tsx
+++ src/app/(site)/tickets/page.tsx
@@ -10,96 +10,115 @@ import GuestTickets from '@/app/components/GuestTickets';
 import { useSearchParams } from 'next/navigation';
 
 export enum Seat {
-  AVAILABLE,
-  BOOKED,
-  CHOOSED,
+	AVAILABLE,
+	BOOKED,
+	CHOOSED,
 }
 
 const TicketsPage: FC = () => {
-  const [ticketCount, setTicketCount] = useState(0);
-  const [seatsArray, setSeatsArray] = useState<[] | { status: Seat; seatNumber: number }[]>([]);
-  const [continueGuest, setContinueGuest] = useState(false);
+	const [ticketCount, setTicketCount] = useState(0);
+	const [seatsArray, setSeatsArray] = useState<
+		[] | { status: Seat; seatNumber: number }[]
+	>([]);
+	const [continueGuest, setContinueGuest] = useState(false);
 
-  const searchParams = useSearchParams();
-  const screeningId = searchParams.get('screening');
-  const screeningDate = searchParams.get('date');
-  const movieTitle = searchParams.get('movie');
-  const movieImg = searchParams.get('img');
+	const searchParams = useSearchParams();
+	const screeningId = searchParams.get('screening');
+	const screeningDate = searchParams.get('date');
+	const movieTitle = searchParams.get('movie');
+	const movieImg = searchParams.get('img');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch(`http://localhost:3000/api/tickets/${screeningId}`, {
-        next: { revalidate: 1 },
-      });
+	useEffect(() => {
+		const fetchData = async () => {
+			const res = await fetch(
+				`http://localhost:3000/api/tickets/${screeningId}`,
+				{
+					next: { revalidate: 1 },
+				}
+			);
 
-      const payload = await res.json();
+			const payload = await res.json();
 
-      let numberOfSeats = 48;
-      let initialSeatsArray = [];
-      let bookedSeatsArray: number[] = payload.map((ticket: { status: number; seat: number }) => {
-        return ticket.seat;
-      });
+			let numberOfSeats = 48;
+			let initialSeatsArray = [];
+			let bookedSeatsArray: number[] = payload.map(
+				(ticket: { status: number; seat: number }) => {
+					return ticket.seat;
+				}
+			);
 
-      for (let i = 1; i <= numberOfSeats; i++) {
-        if (bookedSeatsArray.includes(i)) {
-          initialSeatsArray.push({ status: Seat.BOOKED, seatNumber: i });
-        } else {
-          initialSeatsArray.push({ status: Seat.AVAILABLE, seatNumber: i });
-        }
-      }
-      setSeatsArray(initialSeatsArray);
-    };
+			for (let i = 1; i <= numberOfSeats; i++) {
+				if (bookedSeatsArray.includes(i)) {
+					initialSeatsArray.push({ status: Seat.BOOKED, seatNumber: i });
+				} else {
+					initialSeatsArray.push({ status: Seat.AVAILABLE, seatNumber: i });
+				}
+			}
+			setSeatsArray(initialSeatsArray);
+		};
 
-    fetchData();
-  }, []);
+		fetchData();
+	}, []);
 
-  function availableSeats() {
-    let numberOfBookedSeats = 0;
-    for (let i = 0; i < seatsArray.length; i++) {
-      if (seatsArray[i].status == Seat.BOOKED) {
-        numberOfBookedSeats++;
-      }
-    }
-    let availableSeats = seatsArray.length - numberOfBookedSeats;
-    return availableSeats;
-  }
+	function availableSeats() {
+		let numberOfBookedSeats = 0;
+		for (let i = 0; i < seatsArray.length; i++) {
+			if (seatsArray[i].status == Seat.BOOKED) {
+				numberOfBookedSeats++;
+			}
+		}
+		let availableSeats = seatsArray.length - numberOfBookedSeats;
+		return availableSeats;
+	}
 
-  const handleGuest = () => {
-    setContinueGuest(true);
-  };
-  if (continueGuest) {
-    return <GuestTickets />;
-  } else {
-    return (
-      <section className="sec-cont tickets-container">
-        <h1>Biljettbokning</h1>
+	const handleGuest = () => {
+		setContinueGuest(true);
+	};
+	if (continueGuest) {
+		return <GuestTickets />;
+	} else {
+		return (
+			<section className="sec-cont tickets-container">
+				<h1>Biljettbokning</h1>
 
-        <TicketMovieInfo title={movieTitle} img={movieImg} date={screeningDate} />
-        <TicketCount
-          ticketCount={ticketCount}
-          onClickMinus={() => setTicketCount(ticketCount != 0 ? ticketCount - 1 : ticketCount)}
-          onClickPlus={() => {
-            let freeSeats = availableSeats();
-            setTicketCount(ticketCount < freeSeats ? ticketCount + 1 : ticketCount);
-          }}
-        />
-        <ChooseSeats
-          seatsArray={seatsArray}
-          ticketCount={ticketCount}
-          onSetSeatsArray={(newArray) => setSeatsArray(newArray)}
-        />
-        <PaymentSum ticketCount={ticketCount} />
-        <Link href="/login" className="primary-btn">
-          Logga in
-        </Link>
-        <button className="primary-btn guest-btn" onClick={handleGuest}>
-          Gäst
-        </button>
-        <Link href="/" className="cancel-btn">
-          Avbryt
-        </Link>
-      </section>
-    );
-  }
+				<TicketMovieInfo
+					title={movieTitle}
+					img={movieImg}
+					date={screeningDate}
+				/>
+				<TicketCount
+					ticketCount={ticketCount}
+					onClickMinus={() =>
+						setTicketCount(ticketCount != 0 ? ticketCount - 1 : ticketCount)
+					}
+					onClickPlus={() => {
+						let freeSeats = availableSeats();
+						setTicketCount(
+							ticketCount < freeSeats ? ticketCount + 1 : ticketCount
+						);
+					}}
+				/>
+				<ChooseSeats
+					seatsArray={seatsArray}
+					ticketCount={ticketCount}
+					onSetSeatsArray={(newArray) => setSeatsArray(newArray)}
+				/>
+				<PaymentSum ticketCount={ticketCount} />
+				<Link href="/login" className="primary-btn">
+					Logga in
+				</Link>
+				<button
+					data-testid="guest-btn"
+					className="primary-btn guest-btn"
+					onClick={handleGuest}
+				>
+					Gäst
+				</button>
+				<Link href="/" className="cancel-btn">
+					Avbryt
+				</Link>
+			</section>
+		);
+	}
 };
 export default TicketsPage;
