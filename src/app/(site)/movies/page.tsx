0d9c4d59d3610833362105c4db95cb662ac541d9
Fabrithegreat <32011023+MrFabri@users.noreply.github.com>
2023-05-20 11:28:24 +0200

    not-found, layout, (home) page  moved to app folder, added error handeling for /movies/id

--- src/app/(site)/movies/page.tsx
+++ src/app/(site)/movies/page.tsx
@@ -1,33 +1,38 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default async function Movies() {
+async function getMovies(): Promise<any | null> {
 	const res = await fetch('http://localhost:3000/api/movies', {
 		next: { revalidate: 1 },
 	});
+	if (!res.ok) return null;
+	return await res.json();
+}
 
-	const payload = await res.json();
-
+export default async function Movies() {
+	const movies = await getMovies();
 	return (
 		<div className="sec-cont movies-cont">
 			<h1>Alla filmer</h1>
 			<div className="separation" />
 			<ul className="movie-list">
-				{payload.map((movie: any) => {
-					return (
-						<li key={movie._id} className="movie-item">
-							<Link href={`/movies/${movie._id}`}>
-								<Image
-									src={movie.assets[0]}
-									alt="Film omslagsbild"
-									width={200}
-									height={300}
-								/>
-								<h4 className="movie-item-text">{movie.name}</h4>
-							</Link>
-						</li>
-					);
-				})}
+				{!movies && <p style={{ color: 'white' }}>No movies</p>}
+				{movies &&
+					movies.map((movie: any) => {
+						return (
+							<li key={movie._id} className="movie-item">
+								<Link href={`/movies/${movie._id}`}>
+									<Image
+										src={movie.assets[0]}
+										alt="Film omslagsbild"
+										width={200}
+										height={300}
+									/>
+									<h4 className="movie-item-text">{movie.name}</h4>
+								</Link>
+							</li>
+						);
+					})}
 			</ul>
 		</div>
 	);
